fc73b13a5035a091d6bcc8c7965af8915d72fea2
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-04-03 19:15:41 +0200

    Reusable components

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include "components/header.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taylor Swift Database</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-</head>
+<h1 class="title is-1">Taylor Swift Database</h1>
 
-<body>
-    <h1 class="title is-1">Taylor Swift Database</h1>
-</body>
-
-</html>
+<?php
+    include "components/footer.php";
+?>
