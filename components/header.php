e599077263fe76d2f9932c78833ef772ef4cd9ad
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-04-06 23:51:33 +0200

    first version of index page

--- components/header.php
+++ components/header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="background-color: rgb(209,182,162);">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset=" UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taylor Swift Database</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
 
-<body>
-    <h1 class="title is-1"><a class="has-text-black" href="index.php">Taylor Swift Database</a> </h1>
+<body class="my-5 mx-3">
+    <header class="columns is-centered">
+        <h1 class="title is-1 is-half"><a class="has-text-black" href="index.php">Taylor Swift Database</a> </h1>
+    </header>
--- index.php
+++ index.php
@@ -2,15 +2,69 @@
     include "components/header.php";
 ?>
 
-<a href="http://localhost/TaylorDB/album.php?albumid=1">Taylor Swift</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=2">Fearless (Taylor's Version)</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=3">Speak Now (Deluxe)</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=4">Red (Taylor's Version)</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=5">1898 (Deluxe)</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=6">reputation</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=7">Lover</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=8">folklore (deluxe version)</a> <br>
-<a href="http://localhost/TaylorDB/album.php?albumid=9">evermore (deluxe version)</a>
+<section>
+    <div class="columns is-8 has-text-centered">
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=1">
+                <img src="img/Debut.jpg" alt="">
+                <h3>Taylor Swift</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=2">
+                <img src="img/Fearless.jpg" alt="">
+                <h3>Fearless (Taylor's Version)</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=3">
+                <img src="img/SpeakNow.png" alt="">
+                <h3>Speak Now (Deluxe)</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=4">
+                <img src="img/Red.jpg" alt="">
+                <h3>Red (Taylor's Version)</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=5">
+                <img src="img/1989.jpg" alt="">
+                <h3>1898 (Deluxe)</h3>
+            </a>
+        </div>
+    </div>
+    <div class="columns is-1 has-text-centered">
+        <div class="column is-offset-1">
+            <a href="http://localhost/TaylorDB/album.php?albumid=6">
+                <img src="img/Reputation.jpg" alt="">
+                <h3>reputation</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=7">
+                <img src="img/Lover.jpg" alt="">
+                <h3>Lover</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=8">
+                <img src="img/Folklore.jpg" alt="">
+                <h3>folklore (deluxe version)</h3>
+            </a>
+        </div>
+        <div class="column">
+            <a href="http://localhost/TaylorDB/album.php?albumid=9">
+                <img src="img/Evermore.jpg" alt="">
+                <h3>evermore (deluxe version)</h3>
+            </a>
+        </div>
+        <div class="column is-1"></div>
+    </div>
+
+
+</section>
 
 <?php
     include "components/footer.php";
