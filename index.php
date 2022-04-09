e2200c662672058aee110c4cc75a9f0ac934edad
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-04-09 17:55:05 +0200

    Album cover shadows

--- index.php
+++ index.php
@@ -6,31 +6,31 @@
     <div class="columns is-8 has-text-centered">
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=1">
-                <img src="img/Debut.jpg" alt="">
+                <img src="img/Debut.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">Taylor Swift</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=2">
-                <img src="img/Fearless.jpg" alt="">
+                <img src="img/Fearless.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">Fearless (Taylor's Version)</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=3">
-                <img src="img/SpeakNow.png" alt="">
+                <img src="img/SpeakNow.png" class="album-cover" alt="">
                 <h3 class="is-size-5">Speak Now (Deluxe)</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=4">
-                <img src="img/Red.jpg" alt="">
+                <img src="img/Red.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">Red (Taylor's Version)</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=5">
-                <img src="img/1989.jpg" alt="">
+                <img src="img/1989.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">1898 (Deluxe)</h3>
             </a>
         </div>
@@ -38,25 +38,25 @@
     <div class="columns is-1 has-text-centered">
         <div class="column is-offset-1">
             <a href="http://localhost/TaylorDB/album.php?albumid=6">
-                <img src="img/Reputation.jpg" alt="">
+                <img src="img/Reputation.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">reputation</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=7">
-                <img src="img/Lover.jpg" alt="">
+                <img src="img/Lover.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">Lover</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=8">
-                <img src="img/Folklore.jpg" alt="">
+                <img src="img/Folklore.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">folklore (deluxe version)</h3>
             </a>
         </div>
         <div class="column">
             <a href="http://localhost/TaylorDB/album.php?albumid=9">
-                <img src="img/Evermore.jpg" alt="">
+                <img src="img/Evermore.jpg" class="album-cover" alt="">
                 <h3 class="is-size-5">evermore (deluxe version)</h3>
             </a>
         </div>
