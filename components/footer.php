f4d2f0a0342b3ac01b1b66e02d791ff012c7c739
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-04-15 19:17:50 +0200

    about page modal

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,24 @@
+<button class="js-modal-trigger button is-rounded is-medium info-btn" data-target="modal-info">
+    ?
+</button>
+
+<div id="modal-info" class="modal">
+    <div class="modal-background"></div>
+
+    <div class="modal-content">
+        <div class="box">
+            <h5 class="title has-text-weight-light	">About this page</h5>
+            <p class="about-info">This page was inspired by <a href="https://shaynak.github.io/taylor-swift/">Taylor
+                    Swift lyrics
+                    searcher</a> made by <a href="https://shaynak.github.io/">Shayna Kothari</a>. I also used her <a
+                    href="https://github.com/shaynak/taylor-swift-lyrics">scraped lyrics</a>.</p>
+        </div>
+    </div>
+
+    <button class="modal-close is-large" aria-label="close"></button>
+</div>
+
+<script src="components/modal.js"></script>
 </body>
 
 </html>
