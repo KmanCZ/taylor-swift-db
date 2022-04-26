d65f8f47e0499b837c2a9debfe915da4e825477b
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-04-26 16:00:12 +0200

    Cleaning of uninportant lines in class autoloader

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -5,8 +5,11 @@ class Dbh {
     protected function connect() {
         try {
             $username = "root";
+            //$username = "epiz_31596274";
             $password = "";
+            //$password = "RrgfskFTaQhF";
             $dbh = new PDO("mysql:host=localhost;dbname=taylor-swift-db", $username, $password);
+            //$dbh = new PDO("mysql:host=sql113.epizy.com;dbname=epiz_31596274_TaylorSwiftDb", $username, $password);
             $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $dbh;
         } catch (PDOException $e) {
